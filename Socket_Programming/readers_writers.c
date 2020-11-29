1abb50e1c6dfb2da69f94ca96dc69eb31adb3c22
praffulla <praffullapandey.05@gmail.com>
2020-11-29 17:54:45 +0530

    Create readers_writers.c

---
+++ Socket_Programming/readers_writers.c
@@ -0,0 +1,77 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+
+/*Writer is pretty straight forward. Acquire a write lock so that another writers can't enter it.
+Readers are little tricky. How to make sure that if one writer is inside, reader should not enter.
+Straight forward right? Just look for wrt lock and if it acquired do not read.
+But this creates another issue where more than one readers wants to go inside.
+
+So solution is that let the first reader look of write lock and if it enters to read, subsequent reader should not look for write lock then becuase 
+they know R0 has acquired the lock. So logic should be if reader count == 1 then acquire the lock else do not. But now assume two readers threads
+are running and they want into reader function and now they both do rdr++ and if (rdr_count == 1) it will mess up. So another lock to make sure 
+that once previous reader has done reader count maintainace it can signal another reader to enter*/
+
+sem_t wrt;
+pthread_mutex_t mutex;
+int cnt = 1;
+int numreader = 0;
+
+void *writer(void *wno)
+{   
+    sem_wait(&wrt);
+    cnt = cnt*2;
+    printf("Writer %d modified cnt to %d\n",(*((int *)wno)),cnt);
+    sem_post(&wrt);
+
+}
+void *reader(void *rno)
+{   
+    // Reader acquire the lock before modifying numreader
+    pthread_mutex_lock(&mutex);
+    numreader++;
+    if(numreader == 1) {
+        sem_wait(&wrt); // If this id the first reader, then it will block the writer
+    }
+    pthread_mutex_unlock(&mutex);
+    // Reading Section
+    printf("Reader %d: read cnt as %d\n",*((int *)rno),cnt);
+
+    // Reader acquire the lock before modifying numreader
+    pthread_mutex_lock(&mutex);
+    numreader--;
+    if(numreader == 0) {
+        sem_post(&wrt); // If this is the last reader, it will wake up the writer.
+    }
+    pthread_mutex_unlock(&mutex);
+}
+
+int main()
+{   
+
+    pthread_t read[10],write[5];
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&wrt,0,1);
+
+    int a[10] = {1,2,3,4,5,6,7,8,9,10}; //Just used for numbering the producer and consumer
+
+    for(int i = 0; i < 10; i++) {
+        pthread_create(&read[i], NULL, (void *)reader, (void *)&a[i]);
+    }
+    for(int i = 0; i < 5; i++) {
+        pthread_create(&write[i], NULL, (void *)writer, (void *)&a[i]);
+    }
+
+    for(int i = 0; i < 10; i++) {
+        pthread_join(read[i], NULL);
+    }
+    for(int i = 0; i < 5; i++) {
+        pthread_join(write[i], NULL);
+    }
+
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&wrt);
+
+    return 0;
+    
+}
